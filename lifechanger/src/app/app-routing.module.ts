7072f35fb69e1b55dd9ddbd1a508e043f4c2ab01
Oksana Kolisnyk <zazera777@gmail.com>
2024-04-06 23:32:29 +0300

    Added routing to header navigation links, renamed title in index.html

--- lifechanger/src/app/app-routing.module.ts
+++ lifechanger/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HeroComponent },
+  // The below names of the Components will be changed
+  { path: 'about', component: HeroComponent },
+  { path: 'tech-stack', component: HeroComponent },
+  { path: 'projects', component: HeroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
