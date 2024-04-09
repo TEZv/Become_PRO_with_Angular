ed4b66eaf65df4723b31e1c4dff0a9847498421f
Oksana.Kolisnyk.22y.0 <zazera777@gmail.com>
2024-04-09 14:48:09 +0300

    Adjusted styles in header and footer components, added and adjusted Hero component, improved the overall page view (#5)
    * feat: initial creation of header component
    
    * Adjusted styles to header, added dark styles to styles.css
    
    * Added styles, BEM class names, made dynamic measurements, adjusted images names
    
    * Deleted unnecessary comments in header.comp.css
    
    * Deleted unnecessary comments in styles.css, deleted rules in header.comp.css that are global
    
    * Adjusted slider in header.comp.css, deleted div with id='home' in header.comp.html
    
    * Adjusted the style rules from 3 components, improved overall view
    
    * Accept changes of the style rules from 3 components: footer, header, hero
    
    * Adjusted BEM names in hero.html component, added styles nesting
    
    * Added routing to header navigation links, renamed title in index.html
    
    * Added configuration routing to header navigation links
    
    * Repaired routing according to the comments, added <router-outlet>, replaced <br /> with display: block
    
    * Repaired home routing according to the comment
    
    ---------
    
    Co-authored-by: Ruslan Dzobko <dnepr.ukrainec@gmail.com>

--- lifechanger/src/app/app.module.ts
+++ lifechanger/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent,
   ],
   imports: [
     BrowserModule,
