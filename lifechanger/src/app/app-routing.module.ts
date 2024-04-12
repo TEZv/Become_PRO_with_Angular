a29576da32872f34214d22dccaee188a04f41d28
Oksana Kolisnyk <zazera777@gmail.com>
2024-04-12 20:58:56 +0300

    Accepted current changes and merged

--- lifechanger/src/app/app-routing.module.ts
+++ lifechanger/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
-<<<<<<< HEAD
 import { ProjectsComponent } from './projects/projects.component';
-=======
->>>>>>> ed4b66eaf65df4723b31e1c4dff0a9847498421f
 
 const routes: Routes = [
   { path: '', component: HeroComponent },
@@ -12,11 +9,7 @@ const routes: Routes = [
   // The below names of the Components will be changed
   { path: 'about', component: HeroComponent },
   { path: 'tech-stack', component: HeroComponent },
-<<<<<<< HEAD
   { path: 'projects', component: ProjectsComponent }
-=======
-  { path: 'projects', component: HeroComponent }
->>>>>>> ed4b66eaf65df4723b31e1c4dff0a9847498421f
 ];
 
 @NgModule({
