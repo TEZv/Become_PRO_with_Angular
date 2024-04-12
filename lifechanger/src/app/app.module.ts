075d427439d40e053bdc6b4270a6eff25f46bcae
Ruslan Dzobko <dnepr.ukrainec@gmail.com>
2024-04-13 00:27:15 +0300

    created stack component

--- lifechanger/src/app/app.module.ts
+++ lifechanger/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { StackComponent } from './stack/stack.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeroComponent } from './hero/hero.component';
     FooterComponent,
     HeaderComponent,
     HeroComponent,
+    StackComponent,
   ],
   imports: [
     BrowserModule,
