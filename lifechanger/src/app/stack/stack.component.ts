6344494f53aa3983cfc48c345610bc469fc72e78
Oksana Kolisnyk <zazera777@gmail.com>
2024-04-14 13:50:26 +0300

    Adjusted the stack component

--- lifechanger/src/app/stack/stack.component.ts
+++ lifechanger/src/app/stack/stack.component.ts
@@ -8,18 +8,18 @@ import { Component } from '@angular/core';
 export class StackComponent {
   getImages(): [string, string][] {
     return [
-      ['../../assets/stack/html.svg', 'HTML5 logo'],
-      ['../../assets/stack/css.svg', 'CSS logo'],
-      ['../../assets/stack/js.svg', 'JS logo'],
-      ['../../assets/stack/react.svg', 'React logo'],
-      ['../../assets/stack/redux.svg', 'Redux logo'],
-      ['../../assets/stack/bootstrap.svg', 'Bootstrap logo'],
-      ['../../assets/stack/tailwind.svg', 'Tailwind logo'],
-      ['../../assets/stack/sass.svg', 'SASS logo'],
-      ['../../assets/stack/git.svg', 'Git logo'],
-      ['../../assets/stack/greensock.svg', 'Greensock logo'],
-      ['../../assets/stack/vscode.svg', 'Vscode logo'],
-      ['../../assets/stack/github.svg', 'Github logo'],
+      ['../../assets/stack/html-logo.svg', 'HTML5 logo'],
+      ['../../assets/stack/css-logo.svg', 'CSS logo'],
+      ['../../assets/stack/js-logo.svg', 'JS logo'],
+      ['../../assets/stack/angular-logo.svg', 'Angular logo'],
+      ['../../assets/stack/react-logo.svg', 'React logo'],
+      ['../../assets/stack/bootstrap-logo.svg', 'Bootstrap logo'],
+      ['../../assets/stack/tailwind-logo.svg', 'Tailwind logo'],
+      ['../../assets/stack/sass-logo.svg', 'SASS logo'],
+      ['../../assets/stack/git-logo.svg', 'Git logo'],
+      ['../../assets/stack/vscode-logo.svg', 'VSCode logo'],
+      ['../../assets/stack/github-logo.svg', 'Github logo'],
+      ['../../assets/stack/seo-logo.svg', 'SEO logo'],
     ];
   }
 }
